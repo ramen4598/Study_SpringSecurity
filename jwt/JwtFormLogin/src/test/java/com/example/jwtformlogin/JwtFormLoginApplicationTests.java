4b6447540096e5d05710dc325d3012423e74143e
김의근 <fkaus4598@naver.com>
2024-11-04 13:38:15 +0900

    Init: Jwt Form Login Project Init

---
+++ jwt/JwtFormLogin/src/test/java/com/example/jwtformlogin/JwtFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtformlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtFormLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
