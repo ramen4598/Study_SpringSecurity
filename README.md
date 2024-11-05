92139f5c23098b4a06e0d3c21a5999a9d813cba0
김의근 <fkaus4598@naver.com>
2024-11-05 22:49:40 +0900

    Feat: 단일 토큰 인증 인가 구현

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ## 참고자료
 
 - [Spring Security Samples](https://github.com/spring-projects/spring-security-samples)
+- [유튜브 개발자 유미 - 스피링 시큐리티](https://youtu.be/y0PXQgrkb90?si=s2NO-VwVJJzxPKdF)
--- jwt/JwtFormLogin/src/main/java/com/example/jwtformlogin/config/SecurityConfig.java
+++ jwt/JwtFormLogin/src/main/java/com/example/jwtformlogin/config/SecurityConfig.java
@@ -4,7 +4,13 @@ import com.example.jwtformlogin.domain.jwt.CustomAuthenticationEntryPoint;
 import com.example.jwtformlogin.domain.jwt.JWTFilter;
 import com.example.jwtformlogin.domain.jwt.JWTUtil;
 import com.example.jwtformlogin.domain.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -16,21 +22,51 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
 
+    @Value("${cors.url}")
+    private String corsURL;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        String[] allowedOrigins = Arrays.stream(corsURL.split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
+
         return http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+                                configuration.setAllowedOrigins(Collections.singletonList(corsURL));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true); // 토큰을 주고받을 때는 credentials를 허용해야 함
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L); // 1시간동안 캐싱
+
+                                // 클라이언트에서 Authorization 헤더를 사용할 수 있도록 설정
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        })
+
+                )
                 .authorizeHttpRequests((auth) -> auth
                         // /login, /join, / 경로로 들어오는 요청은 인증이 필요하지 않음
                         .requestMatchers(
